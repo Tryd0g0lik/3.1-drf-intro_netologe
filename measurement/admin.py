e9c264e9a105428defc3332d776ee9be8a6b5783
Tryd0g0lik <work80@mail.ru>
2022-10-22 15:36:01 +0700

    Were rewrited files 'model.py' and also 'admin.py'.  through an admin panel site were the inserted data into db.

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from measurement.models import *
+
+class SensorAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'description')
+	list_display_links = ('id', 'name')
+	list_filter=('name',)
+	search_fields=('name',)
+
+class MeasurementAdmin(admin.ModelAdmin):
+	list_display=('id', 'id_sensor', 'graduses', 'date')
+	list_display_links=('id_sensor',)
+	list_filter=('id_sensor', 'graduses', 'date')
+	search_fields=('id_sensor', 'graduses', 'date')
+
+admin.site.register(Sensor, SensorAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
