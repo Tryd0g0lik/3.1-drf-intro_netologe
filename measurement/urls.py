06430e58b7c7542660a77f0106144a448c49f460
Tryd0g0lik <work80@mail.ru>
2022-11-07 08:16:08 +0700

    Re first commit on the branch v-3

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from rest_framework.routers import  DefaultRouter
 from measurement.views import *
+
+router = DefaultRouter()
 
 urlpatterns = [
 	path('', api_gradus.as_view()),
@@ -9,3 +11,5 @@ urlpatterns = [
 	path('meas/<pk>/', MeasurementViewSet.as_view()),
 	path('meas/', MeasurementViewSet.as_view()),
 ]
+
+
